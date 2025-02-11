ce9f25a884a3aa6bca657b91ca427c04a035601e
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2025-02-11 11:32:44 +0100

    replaced menu for menubar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { MenuModule } from 'primeng/menu';
 
+import { MenubarModule } from 'primeng/menubar';
+
 @NgModule({
-  exports: [MenuModule],
+  exports: [MenuModule, MenubarModule],
 })
 export class PrimeNgModule {}

1bf43a1410804a65e8fde2add24a7bfad8e0a49a
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2025-02-11 16:41:03 +0100

    changed language Locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -6,6 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+
+//Configuración del locale de la app
+import localeEsHN from '@angular/common/locales/es-HN';
+import localeFrCA from '@angular/common/locales/fr-CA';
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsHN); //o todos los idiomas que querríamos manejar en esta app
+registerLocaleData(localeFrCA); //o todos los idiomas que querríamos manejar en esta app
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +24,12 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-HN',
+    }, //aquí sería para una configuración global, excepto los Pipes que yo he especificado otra zona local
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

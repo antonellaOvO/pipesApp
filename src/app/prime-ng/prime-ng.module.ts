e0811e4538d258be6596f8fffbe6b395308598cc
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2025-02-12 18:44:00 +0100

    added table Heroes PrimeNg

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -8,6 +8,7 @@ import { FieldsetModule } from 'primeng/fieldset';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
 import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
@@ -18,6 +19,7 @@ import { ToolbarModule } from 'primeng/toolbar';
     InputTextModule,
     MenubarModule,
     PanelModule,
+    TableModule,
     ToolbarModule,
   ],
 })

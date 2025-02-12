983bebecbdd1e138f2ee50588908d8085ad74e24
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2025-02-12 13:03:49 +0100

    added asyncPipe on uncommon-page Component

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -49,4 +50,17 @@ export class UncommonPageComponent {
     age: 36,
     address: 'Ottawa, Canada',
   };
+
+  //Async Pipe
+
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap((value) => console.log('tap', value))
+  );
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.');
+      console.log('Tenemos data en la promesa.');
+    }, 3500);
+  });
 }
